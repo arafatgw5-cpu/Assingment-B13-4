fe383e327abd929a3308b1905c92be5b674da439
arafatgw5-cpu <arafatgw5@gmail.com>
2026-02-23 13:49:43 +0600

    socond commit

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const totalElement= document.getElementById('totalCount')
+const interviewCountElement= document.getElementById('interviewCount')
+const rejectedCountElement= document.getElementById('rejectedCount')
+console.log(totalElement,interviewCountElement,rejectedCountElement)
+
+const jobCountElement= document.getElementById("jobCount")
+console.log(jobCountElement.innerText)
+
 let currentTab="all";
-
-
