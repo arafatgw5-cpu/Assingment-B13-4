fec735aa7a8a98f26be00a558fa74439f365045b
arafatgw5-cpu <arafatgw5@gmail.com>
2026-02-23 15:45:00 +0600

    4 commit

--- script.js
+++ script.js
@@ -18,4 +18,14 @@ tabBtns.forEach(btn => {
 function switchTab(Tab){
     currentTab=Tab;
     console.log(Tab)
-}
+};
+
+const emptyState = document.getElementById("emptyState");
+console.log(emptyState);
+emptyState.classList.remove("hidden")
+document.getElementById("emptyIcon").innerHTML =
+  '<img src="jobs.png" class="mx-auto w-24">';
+  document.getElementById("emptyTitle").textContent="No jobs available"
+  document.getElementById("emptySub").textContent="Check back soon for new job opportunities"
+  console.log("show")
+  emptyState.classList.add("hidden");
