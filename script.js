7553fc1e8048a69675fa32948a232082004532a5
arafatgw5-cpu <arafatgw5@gmail.com>
2026-02-23 14:45:49 +0600

    3 commit

--- script.js
+++ script.js
@@ -4,6 +4,18 @@ const rejectedCountElement= document.getElementById('rejectedCount')
 console.log(totalElement,interviewCountElement,rejectedCountElement)
 
 const jobCountElement= document.getElementById("jobCount")
-console.log(jobCountElement.innerText)
+console.log(jobCountElement.innerText);
 
-let currentTab="all";
+let currentTab = "all"
+
+const tabBtns= document.querySelectorAll('.tab-btn');
+console.log(tabBtns.length);
+
+tabBtns.forEach(btn => {
+    console.log(btn.getAttribute("data-tab"))
+    
+});
+function switchTab(Tab){
+    currentTab=Tab;
+    console.log(Tab)
+}
